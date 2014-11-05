ba9db3dde12bf7fa9f9e4b454fc7fefc6e34ed78
justinek <justinek@stanford.edu>
2014-11-04 16:28:16 -0800

    make new prior experiment

---
+++ mturkscripts/priors/prior_experiment_v2.js
@@ -0,0 +1,523 @@
+var NUM_SLIDERS = 11;
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function showSlide(id) {
+  $(".slide").hide();
+  $("#"+id).show();
+}
+
+function random(a,b) {
+  if (typeof b == "undefined") {
+    a = a || 2;
+    return Math.floor(Math.random()*a);
+  } else {
+    return Math.floor(Math.random()*(b-a+1)) + a;
+  }
+}
+
+function clearForm(oForm) {
+  var sliderVar = "";
+  for(var i=0; i<NUM_SLIDERS; i++)
+  {
+    sliderVar = "#slider" + i;
+    $(sliderVar).slider("value", 20);
+    $(sliderVar).css({"background":"#FFFFFF"});
+    $(sliderVar + " .ui-slider-handle").css({
+        "background":"#FAFAFA",
+        "border-color": "#CCCCCC" });
+    sliderVar = "slider" + i;
+    document.getElementById(sliderVar).style.background = "";
+  }
+  
+  var elements = oForm.elements; 
+  
+  oForm.reset();
+
+  for(var i=0; i<elements.length; i++) {
+    field_type = elements[i].type.toLowerCase();
+    switch(field_type) {
+    
+      case "text": 
+      case "password": 
+      case "textarea":
+            case "hidden":	
+        
+        elements[i].value = ""; 
+        break;
+          
+      case "radio":
+      case "checkbox":
+          if (elements[i].checked) {
+            elements[i].checked = false; 
+        }
+        break;
+  
+      case "select-one":
+      case "select-multi":
+                  elements[i].selectedIndex = -1;
+        break;
+  
+      default: 
+        break;
+    }
+  }
+}
+
+Array.prototype.random = function() {
+  return this[random(this.length)];
+}
+
+function setQuestion(array) {
+    var i = random(0, array.length - 1);
+    var q = array[i];
+    return q;
+}
+
+function shuffle(array) {
+  var currentIndex = array.length
+    , temporaryValue
+    , randomIndex
+    ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+function shuffledArray(arrLength)
+{
+  var j, tmp;
+  var arr = new Array(arrLength);
+  for (i = 0; i < arrLength; i++)
+  {
+    arr[i] = i;
+  }
+  for (i = 0; i < arrLength-1; i++)
+  {
+    j = Math.floor((Math.random() * (arrLength - 1 - i)) + 0.99) + i;
+    tmp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = tmp;
+  }
+  return arr;
+}
+
+function shuffledSampleArray(arrLength, sampleLength)
+{
+  var arr = shuffledArray(arrLength);
+  var beginIndex = Math.floor(Math.random() * (arrLength-sampleLength+1));
+  return arr.slice(beginIndex, beginIndex+sampleLength);
+}
+
+function getRadioCheckedValue(formNum, radio_name)
+{
+   var oRadio = document.forms[formNum].elements[radio_name];
+   for(var i = 0; i < oRadio.length; i++)
+   {
+      if(oRadio[i].checked)
+      {
+         return oRadio[i].value;
+      }
+   }
+   return '';
+}
+
+function randomizeSharpOffset()
+{
+  
+  var r = Math.floor((Math.random()*6)+1);
+  if (r < 4) { return r; }
+  else { return 3-r; }
+  /*
+  var r = Math.floor((Math.random()*3)+1);
+  return r;
+  */
+}
+
+var allConditions = 
+[
+[
+{"food":"M&M's"},
+{"food":"cookies"},
+{"food":"pies"},
+]
+];
+
+var allNames = ["Ann", "Barbara", "Cathy", "Diana", "Emma", "Fiona", "Albert", "Bob", "Calvin",
+"David", "Edward", "Frank"];
+
+
+var debug = false;
+if(debug) { allConditions = debugConditions; }
+
+
+var numConditions = allConditions.length;
+var chooseCondition = random(0, numConditions-1);
+var allTrialOrders = allConditions[chooseCondition];
+var numTrials = allTrialOrders.length;
+var shuffledOrder = shuffledSampleArray(allTrialOrders.length, numTrials);
+var currentTrialNum = 0;
+var trial;
+var numComplete = 0;
+var currentQuantity;
+var quantitiesArray = [10, 10, 10];
+quantitiesArray = shuffle(quantitiesArray);
+allNames = shuffle(allNames);
+
+showSlide("instructions");
+$("#trial-num").html(numComplete);
+$("#total-num").html(numTrials);
+
+
+var experiment = {
+	condition: chooseCondition + 1,
+	
+  priors0: new Array(numTrials),
+  priors1: new Array(numTrials),
+  priors2: new Array(numTrials),
+  priors3: new Array(numTrials),
+  priors4: new Array(numTrials),
+  priors5: new Array(numTrials),
+  priors6: new Array(numTrials),
+  priors7: new Array(numTrials),
+  priors8: new Array(numTrials),
+  priors9: new Array(numTrials),
+  priors10: new Array(numTrials),
+  
+  
+  orders: new Array(numTrials),
+  foods: new Array(numTrials),
+  quantities: new Array(numTrials),
+  personAs: new Array(numTrials),
+  personBs: new Array(numTrials),
+  
+  gender: "",
+  age:"",
+  income:"",
+  nativeLanguage:"",
+  comments:"",
+  description: function() {
+    showSlide("description");
+    $("#tot-num").html(numTrials);	
+  },
+  end: function() {
+    var gen = getRadioCheckedValue(1, "genderButton");
+    var ag = document.age.ageRange.value;
+    var lan = document.language.nativeLanguage.value;
+    var comm = document.comments.input.value;
+    var incomeVal = document.income.incomeRange.value;
+    experiment.gender = gen;
+    experiment.age = ag;
+    experiment.nativeLanguage = lan;
+    experiment.comments = comm;
+    experiment.income = incomeVal;
+    clearForm(document.forms[1]);
+    clearForm(document.forms[2]);
+    clearForm(document.forms[3]);
+    clearForm(document.forms[4]);
+    clearForm(document.forms[5]);    
+    showSlide("finished");
+    setTimeout(function() {turk.submit(experiment) }, 1500);
+  },
+  next: function() {
+    if (numComplete > 0) {
+      //var price = 0;//parseFloat(document.price.score.value) + parseFloat(document.price.score1.value) / 100.00;
+      
+      var prob0 = parseInt(document.getElementById("hiddenSliderValue0").value) / 40.00;
+      var prob1 = parseInt(document.getElementById("hiddenSliderValue1").value) / 40.00;
+      var prob2 = parseInt(document.getElementById("hiddenSliderValue2").value) / 40.00;
+      var prob3 = parseInt(document.getElementById("hiddenSliderValue3").value) / 40.00;
+      var prob4 = parseInt(document.getElementById("hiddenSliderValue4").value) / 40.00;
+      var prob5 = parseInt(document.getElementById("hiddenSliderValue5").value) / 40.00;
+      var prob6 = parseInt(document.getElementById("hiddenSliderValue6").value) / 40.00;
+      var prob7 = parseInt(document.getElementById("hiddenSliderValue7").value) / 40.00;
+      var prob8 = parseInt(document.getElementById("hiddenSliderValue8").value) / 40.00;
+      var prob9 = parseInt(document.getElementById("hiddenSliderValue9").value) / 40.00;
+      var prob10 = parseInt(document.getElementById("hiddenSliderValue10").value) / 40.00;
+      
+      
+      
+      experiment.priors0[currentTrialNum] = prob0;
+      experiment.priors1[currentTrialNum] = prob1;
+      experiment.priors2[currentTrialNum] = prob2;
+      experiment.priors3[currentTrialNum] = prob3;
+      experiment.priors4[currentTrialNum] = prob4;
+      experiment.priors5[currentTrialNum] = prob5;
+      experiment.priors6[currentTrialNum] = prob6;
+      experiment.priors7[currentTrialNum] = prob7;
+      experiment.priors8[currentTrialNum] = prob8;
+      experiment.priors9[currentTrialNum] = prob9;
+      experiment.priors10[currentTrialNum] = prob10;
+      
+      
+      
+      experiment.orders[currentTrialNum] = numComplete;
+      
+      experiment.foods[currentTrialNum] = trial.food;
+
+      experiment.quantities[currentTrialNum] = currentQuantity;
+      
+        	
+      clearForm(document.forms[0]);
+      clearForm(document.forms[1]);
+    }
+    if (numComplete >= numTrials) {
+    	$('.bar').css('width', (200.0 * numComplete/numTrials) + 'px');
+    	$("#trial-num").html(numComplete);
+    	$("#total-num").html(numTrials);
+    	showSlide("askInfo");
+    } else {
+    	$('.bar').css('width', (200.0 * numComplete/numTrials) + 'px');
+    	$("#trial-num").html(numComplete);
+    	$("#total-num").html(numTrials);
+    	currentTrialNum = numComplete;
+    	trial = allTrialOrders[shuffledOrder[numComplete]];
+
+    	personA = allNames.shift();
+      personB = allNames.shift();
+
+      experiment.personAs[numComplete] = personA;
+      experiment.personBs[numComplete] = personB;
+
+      showSlide("stage");
+      $("#personA").html(personA);
+      $("#personB").html(personB);
+      $("#food1").html(trial.food);
+      $("#food2").html(trial.food);
+      //$("#food3").html(trial.food);
+
+      currentQuantity = quantitiesArray[numComplete];
+      $("#quantity").html(currentQuantity);
+      
+
+      intervals = currentQuantity / 10
+      for (var i = 0; i <= 9; i++)
+      {        
+        if (intervals > 1) {
+          label = (intervals * i).toString() + " ~ " + (intervals * (i+1) - 1).toString();
+        } else {
+          label = (intervals * i).toString()
+        }
+        
+        $("#amount" + i).html(label);
+
+      }
+
+      $("#amount10").html(currentQuantity);
+
+      
+      numComplete++;
+      
+    }
+  }
+}
+
+// scripts for sliders
+$("#slider0").slider({
+               animate: true,
+               orientation: "vertical",
+               max: 40 , min: 0, step: 1, value: 20,
+               slide: function( event, ui ) {
+                   $("#slider0 .ui-slider-handle").css({
+                      "background":"#E0F5FF",
+                      "border-color": "#001F29"
+                   });
+               },
+               change: function( event, ui ) {
+                   $('#hiddenSliderValue0').attr('value', ui.value);
+                   $("#slider0").css({"background":"#99D6EB"});
+                   $("#slider0 .ui-slider-handle").css({
+                     "background":"#667D94",
+                     "border-color": "#001F29" });
+               }});
+$("#slider1").slider({
+               animate: true,
+               orientation: "vertical",
+               max: 40 , min: 0, step: 1, value: 20,
+               slide: function( event, ui ) {
+                   $("#slider1 .ui-slider-handle").css({
+                      "background":"#E0F5FF",
+                      "border-color": "#001F29"
+                   });
+               },
+               change: function( event, ui ) {
+                   $('#hiddenSliderValue1').attr('value', ui.value);
+                   $("#slider1").css({"background":"#99D6EB"});
+                   $("#slider1 .ui-slider-handle").css({
+                     "background":"#667D94",
+                     "border-color": "#001F29" });
+               }});
+$("#slider2").slider({
+               animate: true,
+               orientation: "vertical",
+               max: 40 , min: 0, step: 1, value: 20,
+               slide: function( event, ui ) {
+                   $("#slider2 .ui-slider-handle").css({
+                      "background":"#E0F5FF",
+                      "border-color": "#001F29"
+                   });
+               },
+               change: function( event, ui ) {
+                   $('#hiddenSliderValue2').attr('value', ui.value);
+                   $("#slider2").css({"background":"#99D6EB"});
+                   $("#slider2 .ui-slider-handle").css({
+                     "background":"#667D94",
+                     "border-color": "#001F29" });
+               }});
+$("#slider3").slider({
+               animate: true,
+               orientation: "vertical",
+               max: 40 , min: 0, step: 1, value: 20,
+               slide: function( event, ui ) {
+                   $("#slider3 .ui-slider-handle").css({
+                      "background":"#E0F5FF",
+                      "border-color": "#001F29"
+                   });
+               },
+               change: function( event, ui ) {
+                   $('#hiddenSliderValue3').attr('value', ui.value);
+                   $("#slider3").css({"background":"#99D6EB"});
+                   $("#slider3 .ui-slider-handle").css({
+                     "background":"#667D94",
+                     "border-color": "#001F29" });
+               }});
+$("#slider4").slider({
+               animate: true,
+               orientation: "vertical",
+               max: 40 , min: 0, step: 1, value: 20,
+               slide: function( event, ui ) {
+                   $("#slider4 .ui-slider-handle").css({
+                      "background":"#E0F5FF",
+                      "border-color": "#001F29"
+                   });
+               },
+               change: function( event, ui ) {
+                   $('#hiddenSliderValue4').attr('value', ui.value);
+                   $("#slider4").css({"background":"#99D6EB"});
+                   $("#slider4 .ui-slider-handle").css({
+                     "background":"#667D94",
+                     "border-color": "#001F29" });
+               }});
+$("#slider5").slider({
+               animate: true,
+               orientation: "vertical",
+               max: 40 , min: 0, step: 1, value: 20,
+               slide: function( event, ui ) {
+                   $("#slider5 .ui-slider-handle").css({
+                      "background":"#E0F5FF",
+                      "border-color": "#001F29"
+                   });
+               },
+               change: function( event, ui ) {
+                   $('#hiddenSliderValue5').attr('value', ui.value);
+                   $("#slider5").css({"background":"#99D6EB"});
+                   $("#slider5 .ui-slider-handle").css({
+                     "background":"#667D94",
+                     "border-color": "#001F29" });
+               }});
+               
+$("#slider6").slider({
+               animate: true,
+               orientation: "vertical",
+               max: 40 , min: 0, step: 1, value: 20,
+               slide: function( event, ui ) {
+                   $("#slider6 .ui-slider-handle").css({
+                      "background":"#E0F5FF",
+                      "border-color": "#001F29"
+                   });
+               },
+               change: function( event, ui ) {
+                   $('#hiddenSliderValue6').attr('value', ui.value);
+                   $("#slider6").css({"background":"#99D6EB"});
+                   $("#slider6 .ui-slider-handle").css({
+                     "background":"#667D94",
+                     "border-color": "#001F29" });
+               }});
+
+$("#slider7").slider({
+               animate: true,
+               orientation: "vertical",
+               max: 40 , min: 0, step: 1, value: 20,
+               slide: function( event, ui ) {
+                   $("#slider7 .ui-slider-handle").css({
+                      "background":"#E0F5FF",
+                      "border-color": "#001F29"
+                   });
+               },
+               change: function( event, ui ) {
+                   $('#hiddenSliderValue7').attr('value', ui.value);
+                   $("#slider7").css({"background":"#99D6EB"});
+                   $("#slider7 .ui-slider-handle").css({
+                     "background":"#667D94",
+                     "border-color": "#001F29" });
+               }});
+
+$("#slider8").slider({
+               animate: true,
+               orientation: "vertical",
+               max: 40 , min: 0, step: 1, value: 20,
+               slide: function( event, ui ) {
+                   $("#slider8 .ui-slider-handle").css({
+                      "background":"#E0F5FF",
+                      "border-color": "#001F29"
+                   });
+               },
+               change: function( event, ui ) {
+                   $('#hiddenSliderValue8').attr('value', ui.value);
+                   $("#slider8").css({"background":"#99D6EB"});
+                   $("#slider8 .ui-slider-handle").css({
+                     "background":"#667D94",
+                     "border-color": "#001F29" });
+               }});
+
+$("#slider9").slider({
+               animate: true,
+               orientation: "vertical",
+               max: 40 , min: 0, step: 1, value: 20,
+               slide: function( event, ui ) {
+                   $("#slider9 .ui-slider-handle").css({
+                      "background":"#E0F5FF",
+                      "border-color": "#001F29"
+                   });
+               },
+               change: function( event, ui ) {
+                   $('#hiddenSliderValue9').attr('value', ui.value);
+                   $("#slider9").css({"background":"#99D6EB"});
+                   $("#slider9 .ui-slider-handle").css({
+                     "background":"#667D94",
+                     "border-color": "#001F29" });
+               }});
+               
+
+$("#slider10").slider({
+               animate: true,
+               orientation: "vertical",
+               max: 40 , min: 0, step: 1, value: 20,
+               slide: function( event, ui ) {
+                   $("#slider10 .ui-slider-handle").css({
+                      "background":"#E0F5FF",
+                      "border-color": "#001F29"
+                   });
+               },
+               change: function( event, ui ) {
+                   $('#hiddenSliderValue10').attr('value', ui.value);
+                   $("#slider10").css({"background":"#99D6EB"});
+                   $("#slider10 .ui-slider-handle").css({
+                     "background":"#667D94",
+                     "border-color": "#001F29" });
+               }});
+
